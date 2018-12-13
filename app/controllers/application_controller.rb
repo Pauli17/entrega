6fea19c19563488ad25d6200d6dd422e348b5f9c
Pauli <paula.g.17@hotmail.com>
2018-12-13 03:48:47 -0300

    andan fechas venc

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  
-  before_action :configure_permitted_parameters, if: :devise_controller? 
- 
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
  protected
- def configure_permitted_parameters 
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:date_venc,:age,:cod,:card_Number,
+ def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:date_venc_month,:date_venc_year,:age,:cod,:card_Number,
     	:name,:image,:date_of_birth,:email,:lastname,:password,:password_confirmation,
     	:card_Marca,:user_type_id])
    devise_parameter_sanitizer.permit(:account_update, keys: [:date_venc,:age,:cod,:card_Number,
     	:name,:image,:date_of_birth,:email,:lastname,:password,:password_confirmation,
-    	:card_Marca, :user_type_id])
+    	:card_Marca, :user_type_id,:date_venc_month,:date_venc_year])
 end
 end
